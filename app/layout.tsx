9458ce0f041b557ffc037e50116e2f07bd945f56
Даниил Боколяр <bokolyar2la@gmail.com>
2026-06-05 21:52:07 +0300

    feat: add site favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,13 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "/"
   },
+  icons: {
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.png", sizes: "512x512", type: "image/png" }
+    ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }]
+  },
   openGraph: {
     title: "Влюди — куда сходить и чем заняться в Туле",
     description:
