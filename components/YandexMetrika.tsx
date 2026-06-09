5ca63560d4aeaae0240879265894165bd10a03a7
Даниил Боколяр <bokolyar2la@gmail.com>
2026-06-09 15:15:29 +0300

    fix: simplify yandex metrika markup

--- components/YandexMetrika.tsx
+++ components/YandexMetrika.tsx
@@ -1,42 +1,40 @@
 const metrikaId = process.env.NEXT_PUBLIC_YANDEX_METRIKA_ID || "109691466";
 
 export function YandexMetrika() {
+  const metrikaScript = `
+    (function(m,e,t,r,i,k,a){
+      m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+      m[i].l=1*new Date();
+      for (var j = 0; j < document.scripts.length; j++) {
+        if (document.scripts[j].src === r) { return; }
+      }
+      k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js?id=${metrikaId}", "ym");
+
+    ym(${metrikaId}, "init", {
+      ssr: true,
+      webvisor: true,
+      clickmap: true,
+      ecommerce: "dataLayer",
+      referrer: document.referrer,
+      url: location.href,
+      accurateTrackBounce: true,
+      trackLinks: true
+    });
+  `;
+
   return (
     <>
       <script
         id="yandex-metrika"
-        dangerouslySetInnerHTML={{
-          __html: `
-          (function(m,e,t,r,i,k,a){
-            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {
-              if (document.scripts[j].src === r) { return; }
-            }
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-          })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js?id=${metrikaId}", "ym");
-
-          ym(${metrikaId}, "init", {
-            ssr: true,
-            webvisor: true,
-            clickmap: true,
-            ecommerce: "dataLayer",
-            referrer: document.referrer,
-            url: location.href,
-            accurateTrackBounce: true,
-            trackLinks: true
-          });
-        `}
-        }}
+        dangerouslySetInnerHTML={{ __html: metrikaScript }}
       />
       <noscript>
-        <div>
-          <img
-            src={`https://mc.yandex.ru/watch/${metrikaId}`}
-            style={{ position: "absolute", left: "-9999px" }}
-            alt=""
-          />
-        </div>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `<img src="https://mc.yandex.ru/watch/${metrikaId}" style="position:absolute; left:-9999px;" alt="" />`
+          }}
+        />
       </noscript>
     </>
   );
