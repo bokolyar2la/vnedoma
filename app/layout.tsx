db2fa87c93c1e2f70db8a3a81fce438743f27b61
Даниил Боколяр <bokolyar2la@gmail.com>
2026-06-14 23:40:44 +0300

    Improve favicon for Yandex

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
+      { url: "/favicon-120x120.png", sizes: "120x120", type: "image/png" },
       { url: "/favicon.ico", sizes: "any", type: "image/x-icon" },
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
